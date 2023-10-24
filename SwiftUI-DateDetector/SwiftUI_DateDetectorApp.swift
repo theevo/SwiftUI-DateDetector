b0bd79d7aa71cf95961aa68b4f9ba9eb9434cbad
Theo Vora <theo.vora@gmail.com>
2023-10-24 12:24:46 -0500

    Initial Commit

---
+++ SwiftUI-DateDetector/SwiftUI_DateDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DateDetectorApp.swift
+//  SwiftUI-DateDetector
+//
+//  Created by Theo Vora on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DateDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
