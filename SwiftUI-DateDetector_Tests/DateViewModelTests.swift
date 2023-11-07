034c375ff0a0869caa5396efe780c41099ac9137
Theo Vora <theo.vora@gmail.com>
2023-11-06 19:42:10 -0600

    isolate the single assertion that is the focus of the test

--- SwiftUI-DateDetector_Tests/DateViewModelTests.swift
+++ SwiftUI-DateDetector_Tests/DateViewModelTests.swift
@@ -13,8 +13,6 @@ final class DateViewModelTests: XCTestCase {
     func test_yearZero_isNotValid() {
         let vm = buildDateVM("12310000")
         
-        XCTAssertTrue(vm.monthValidity.isValid)
-        XCTAssertTrue(vm.dayValidity.isValid)
         XCTAssertTrue(vm.yearValidity.notValid)
     }
     
@@ -22,32 +20,24 @@ final class DateViewModelTests: XCTestCase {
         let vm = buildDateVM("13312000")
         
         XCTAssertTrue(vm.monthValidity.notValid)
-        XCTAssertTrue(vm.dayValidity.isValid)
-        XCTAssertTrue(vm.yearValidity.isValid)
     }
     
     func test_month00_isNotValid() {
         let vm = buildDateVM("00312000")
         
         XCTAssertTrue(vm.monthValidity.notValid)
-        XCTAssertTrue(vm.dayValidity.isValid)
-        XCTAssertTrue(vm.yearValidity.isValid)
     }
     
     func test_day32_isNotValid() {
         let vm = buildDateVM("05322000")
         
-        XCTAssertTrue(vm.monthValidity.isValid)
         XCTAssertTrue(vm.dayValidity.notValid)
-        XCTAssertTrue(vm.yearValidity.isValid)
     }
     
     func test_day00_isNotValid() {
         let vm = buildDateVM("01002000")
         
-        XCTAssertTrue(vm.monthValidity.isValid)
         XCTAssertTrue(vm.dayValidity.notValid)
-        XCTAssertTrue(vm.yearValidity.isValid)
     }
     
     // MARK: - Helpers
